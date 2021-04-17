58faa992200a4005b53b41ebc5f487fb108096c3
likar-git <kod24713@mail.ru>
2021-04-17 17:08:37 +0300

    Поиск по базе и формирование get-запроса для вывода

--- weather.py
+++ weather.py
@@ -1,11 +1,14 @@
 import json
+import requests
+appid = 'd19b2b422c0755e561aaa39961633ca0'
 #читаем список городов
 with open("C:\\Users\\Павел\\IdeaProjects\\weather\\city.list.json", "r", encoding='utf-8') as read_file:
     city_list = json.load(read_file)
 #ищем нужный город и его id
+city = input()
 for item in city_list:
-    if item['name'] == 'Moscow':
-        city = 'Moscow'
+    if item['name'] == city:
+        city = item['name']
         id = item['id']
         break
 print(city, id)
